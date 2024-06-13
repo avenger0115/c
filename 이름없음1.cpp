a2785ba425cf605bff413dccd60d35d040861c79
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-06-13 20:47:37 +0900

    2024-06-13

---
+++ 이름없음1.cpp
@@ -0,0 +1,59 @@
+//#include <stdio.h>
+//
+//int main(int argc, char*argvs[])
+//{
+//	printf("\'발명왕\'토마스에디슨은 다음과 같이말했다.\n") ;
+//	printf("\"천재는1%%의 영감과 99%%의 노력으로 이루어진다.\"\n") ;	
+//	return 0 ; 
+//}
+//#include <stdio.h>
+//
+//int main(int argc, char*argvs[])
+//{
+//	printf("%d 학년%d ",6,3 ) ;
+//	printf("%f cm %1f kg", 157.56,45.89 ) ;
+//	printf("나의 이름은%s 이고 혈액형은%c 형입니다.", "홍길동", 'A' ) ;
+//	return 0 ;
+//	
+//}
+//#include <stdio.h>
+//
+//int main(){
+//    
+//    int garde = 6;
+//    int classs = 3;
+//    int number = 22;
+//	printf("%d학년%d반 %d ",garde,classs,number ) ;
+//	return 0 ;
+//}
+//#include <stdio.h>
+
+//int main(int argc, char*argvs[])
+//{
+//	printf("%d년%d월 %s \n",2016,3,"알파고 승리" ) ;
+//	printf("%dD %s \n",3,"프린터로 집 짓기" ) ;
+//	printf("%s = %.2lf\n", "원주율",3.14  ) ;
+//	printf("%.3lf km %s\n", 42.195, "마라톤" ) ;
+//	return 0 ;
+//	
+//}
+#include <stdio.h>
+
+int main(int argc, char*argvs[])
+{
+	printf("%s %d %s %d \n", "정수",153,"과",-153 ) ;
+	printf("%s %.3lf %s %.3lf \n","실수",42.195,"와",-42.195 ) ;
+	printf("%s %c %c \n","문자",\'A',\'a'  ) ;
+	printf("%s %s %s \n","문자열", "Artificial lntelligence와 ", "마빈 민스키" ) ;
+	printf("10진수: %d, 8진수: %o , 16진수: %x",255,255,255) ;
+	return 0 ;
+	
+}
+
+
+
+
+
+
+
+
