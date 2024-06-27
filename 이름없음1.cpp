1bec6d6c5cca642e731f8a8b8e8df2e8d6f9190a
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-06-27 20:49:58 +0900

    2024-06-27

--- 이름없음1.cpp
+++ 이름없음1.cpp
@@ -1,78 +1,72 @@
-//#include <stdio.h> 
-//
-//int main( ){
-//	
-//	int a ;
-//	float f1 ;
-//	char ch ;
-//	
-//	a = 15 ;
-//	f1 = 123.456 ;
-//	ch = 'A' ;
-//	printf("%d\n",a);
-//	printf("%f\n",f1);
-//	printf("%c\n",ch);
+//#include <stdio.h>
+//int main()
+//{
+//   int a;
+//   int f1;
+//   int ch;
+//   
+//   scanf("%d %d %d",&a,&f1,&ch ) ;
+//   printf("%d년%d월%d일\n",a,f1,ch ) ;
+//   
+//   
 //	
 //}
 //#include <stdio.h>
-//int main(){
-//	
-//	int num ;
-//	printf("숫자를 하나 입력해주세요. :");
-//	scanf("%d",&num);
-//	printf("%d\n",num); 
-//	
-//}
-//#include <stdio.h>
-//int main(){
-//	
-//	float eye_1 ;
-//	float eye_r ;
-//	printf("양쪽  시력을 입력해주세요.:");
-//	scanf("%f %f",&eye_1,&eye_r);
-//	printf("왼쪽 시력 : %f 오른쪽 시력 : %f", eye_1,eye_r);
-//}
-//#include <stdio.h>
-//int main(){
-//	
-//	int a;
-//	int f1;
-//	int ch;
-//	
-//  scanf("%d %d %d",&a,&f1,&ch);
-//	printf("%d학년 %d반 %d 번호\n",a,f1,ch) ;
-//	
-//}
-//#include <stdio.h>
-//int main(){
-//	
-//    float a ;
-//	float f1 ; 
-//	
-//    scanf("%f %f",&a,&f1);
-//	printf("%f키\n",a);
-//    printf("%f몸무게\n",f1);
-//	
-//}
-//#include <stdio.h>
-//int main(){
-//	
-//	int a, b;
+//int main()
+//{
+//	int a,b ;
 //	scanf("%d %d",&a,&b);
-//	printf("%d\n",a+b);
-//	printf("%d\n",a-b);
-//	printf("%d\n",a*b);
-//	printf("%d\n",a/b);
-//	printf("%d\n",a%b);
-//} 
+//    printf("%d + %d = %d\n",a,b,a+b);
+//    printf("%d - %d = %d\n",a,b,a-b);
+//    printf("%d * %d = %d\n",a,b,a*b);
+//    printf("%d / %d = %d\n",a,b,a/b);
+//	printf("%d %% %d = %d\n",a,b,a%b);
+//
+//	return 0 ;
+//	
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	float a,b ;
+//	scanf("%f %f",&a,&b);
+//    printf("%f + %f = %f\n",a,b,a+b);
+//    printf("%f - %f = %f\n",a,b,a-b);
+//    printf("%f * %f = %f\n",a,b,a*b);
+//    printf("%f / %f = %f\n",a,b,a/b);
+//	
+//	return 0 ;
+//	
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n",10/3);
+//	printf("%d\n",10/3.0);
+//	printf("%f\n",10/3.0);	
+//	
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d",&a);
+//	printf("%d\n",a%10+a/10);
+//
+//}
 #include <stdio.h>
-int main(){
-	
-	char ddd;
-	scanf("%c",&ddd);
-	printf("%c",ddd);
-	
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("%d\n",a/100+a/10%10+a%10);
+    printf("%d\n",(a/100)*(a/10%10)*(a%10));
 }
+
+
+
+
+
 
 
 
