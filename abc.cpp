19612bf43971aaf3cbc4032fd9a52f304f6c7ba0
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-07-06 11:55:11 +0900

    2024-07-06

---
+++ abc.cpp
@@ -0,0 +1,72 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	
+//	a=23;
+//	b=24;
+//	printf("%d\n",a>b);
+//	printf("%d\n",a<b);
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n", 10>5) && (8<3);
+//	printf("%d\n", 10>5) || (8<3);
+//	printf("%d\n", !(10>8));
+//}
+//#include <stdio.h>
+//int main()
+//{
+//   int res;
+//	
+//   res=0&&0;
+//   printf("%d\n",res);
+//   res=0&&1;
+//   printf("%d\n",res);
+//   res=1&&0;
+//   printf("%d\n",res);
+//   res=1&&1;
+//   printf("%d\n",res);
+//   
+//   res=0||0;
+//   printf("%d\n",res);
+//   res=0||1;
+//   printf("%d\n",res);
+//   res=1||0;
+//   printf("%d\n",res);
+//   res=1||1;
+//   printf("%d\n",res);
+//   
+//   printf("%d\n", !res);
+//}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//int main()
+//{
+//   int res ;
+//   
+//   srand(time(NULL));
+//   printf("%d\n",rand());
+//   res = rand();
+//   printf("%d", res);
+//}
+#include <stdio.h>
+int main()
+{
+	int a = 20;
+	
+	a = a+10;
+	printf("a = %d\n",a);
+	a = a*10;
+	printf("a = %d\n",a);
+	a = a-10;
+	printf("a = %d\n",a);
+	a = a/10;
+	printf("a = %d\n",a);
+}
+
+
+
+
