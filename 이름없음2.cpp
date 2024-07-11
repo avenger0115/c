a6fd543b126f2381423fa9cd2c46844714de2484
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-07-11 20:50:57 +0900

    2024-07-11

---
+++ 이름없음2.cpp
@@ -0,0 +1,104 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b;
+//	
+//	a=20;
+//	b=5;
+//	
+//	a+=b;
+//	printf("a=%d\n",a);
+//	a*=b;
+//	printf("a=%d\n",a);
+//	a-=b;
+//	printf("a=%d\n",a);
+//	a/=b;
+//	printf("a=%d\n",a);
+//	a%=b;
+//	printf("a=%d\n",a);
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	
+//	a++;
+//	a++;
+//	printf("a = %d\n",a);
+//	++a;
+//}
+//	++a;
+//	printf("a = %d\n",a);
+//	
+//#include<stdio.h>
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	
+//	int num3 = ++num1+num2++;
+//	
+//	printf("num1 = %d\n",num1);
+//	printf("num2 = %d\n",num2);
+//	printf("num3 = %d\n",num3);
+//	
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 2;
+//	
+//	a-=b;
+//	printf("a = %d\n",a);
+//	a*=b;
+//	printf("a = %d\n",a);
+//	a--;
+//	printf("a = %d\n",a);
+//	a%=b;
+//	printf("a = %d\n",a);
+//	a+=b;
+//	printf("a = %d\n",a);
+//	a++;
+//	printf("a = %d\n",a);
+//	a/=b;
+//	printf("a = %d\n",a);
+//	
+//////}
+////#include<stdio.h>
+////int main()
+////{
+////	int a, b;
+////	
+////	a = 10;
+////	b = a++;
+////	printf("a = %d b = %d\n",a,b);
+////	b = ++a;
+////	printf("a = %d b = %d\n",a,b);
+////	
+////	a = 20;
+////	b = a--;
+////	printf("a = %d b =%d\n",a,b);
+////	b = --a;
+////	printf("a = %d b =%d\n",a,b);
+//}
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	
+	printf("%d\n",n);
+	printf("%d\n",n+=3);
+	printf("%d\n",n*=0);
+	printf("%d\n",n+=5);
+	printf("%d\n",n++);
+	printf("%d\n",++n);
+	printf("%d\n",n%5);
+	printf("%d\n",--n);
+	printf("%d\n",n--);
+	printf("%d\n",n);
+	
+}
+
+
+
