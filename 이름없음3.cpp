f421a92dcad433f108dd6d6266eddef659e6b16a
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-07-13 11:50:52 +0900

    2024-07-13

---
+++ 이름없음3.cpp
@@ -0,0 +1,42 @@
+//#include<stdio.h>
+//int main()
+//{
+//   int i = 1;
+//   int j = 1;	
+//   int result = i++ + --j;
+//   
+//   printf("%d\n",i);
+//   printf("%d\n",j);
+//   printf("%d",result);	
+//	
+//} 
+//#include<stdio.h> 
+//int main()
+//{
+//   int m = 10, n = 5;
+//   int result ;
+//   
+//   result = m++ + --n;
+//   printf("m = %d, n = %d, result = %d \n",m,n, result);
+//   
+//   result = ++m - n--;
+//   printf("m = %d, n = %d, result = %d \n",m,n, result);
+//   
+//   return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a,b ;
+//	scanf("%d %d",&a,&b);
+//	printf("%d\n",a+b);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+    int a,b ;
+	scanf("%d%d",&a,&b);
+	printf("%d\n",a-b);
+	return 0;	
+}
