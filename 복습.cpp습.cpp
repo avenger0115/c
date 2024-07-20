092373bc9dc0259da0202e7b72840be5536a9388
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-07-20 11:46:07 +0900

    2024-07-20

---
+++ 복습.cpp습.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	scanf("%d,",num);
+	
+	if(num%2==0)
+	{
+		printf("%d",num);
+	}
+	else if(num%2==1)
+	{
+		printf("%d",num);
+	}
+	else
+	{
+		printf("0");
+	}
+}
