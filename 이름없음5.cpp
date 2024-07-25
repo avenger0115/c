3f8f2c5c8b1664298f00abe6d802f6f60567366a
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-07-25 20:48:29 +0900

    2024-07-25

---
+++ 이름없음5.cpp
@@ -0,0 +1,101 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int a,b;
+//	scanf("%d %d",&a,&b);
+//	if(a%2==0&&b%2==0)
+//	{
+//		printf("%d",a*b);
+//	}
+//	else if(a%2==1&&b%2==1)
+//	{
+//		printf("%d",a+b);
+//	}
+//	else
+//	{
+//		printf("%d",a-b);
+//	}
+//	    
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i;
+//	
+//	for (i = 1; i <= 10; i++)
+//	{
+//		printf("¾¾Å¥ºêÄÚµù\n");
+//	}
+//	
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i;
+//	
+//	for(i=-10; i <= -1; i++)
+//	{
+//		printf("i = %d\n",i);
+//	}
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i;
+//	
+//	for(i = 100; i >= 0; i=i-10)
+//	{
+//		printf("%d\n",i);
+//	}
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d",&n);
+//	for(i = -n; i <= n; i++)
+//	{
+//		printf("%d ",i);
+//	}
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n,sum=0;
+//	scanf("%d",&n);
+//	for(int i = 1; i <= n; i=1+i)
+//	{
+//		sum+=i;
+//	}
+//	printf("%d",sum);
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d",&n);
+//	for(int i = 9; i >= 1; i=i-1)
+//	{
+//		printf("%d x %d = %d\n",n,i,n*i);
+//	}
+//}
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	for(int i = 1; i <= n; i++)
+	{
+       if(i%3==0)
+	   {
+	   	  printf("%d",i);
+	   }
+	}
+}
+
+
+
+
+
+
+
