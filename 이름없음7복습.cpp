65145bd594e53b6ba5c8155e88d2b8b7dd3ffc5d
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-08-17 11:42:28 +0900

    2024-08-17

---
+++ 이름없음7복습.cpp
@@ -0,0 +1,91 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	i = 1;
+	for(; i<=10;)
+	{
+		printf("%d" ,i);
+		i++;
+	}
+	return 0;
+}
+//#include<stio.h>
+//int main()
+//{
+//	int i;
+//	i = 1;
+//	while(i<=10)
+//	{
+//		printf("%d ",i);
+//		i++;
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	n = 10;
+//	while(n <= 17)
+//	{
+//		printf("%d ",n);
+//		n++;
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	n = 20;
+//	while(n <= 15)
+//	{
+//		printf("%d ",n);
+//		n--;
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n,m;
+//	scanf("%d %d",&n,&m);
+//	int j=1;
+//	while(j<m)
+//	{
+//		printf("%d ",i);
+//		i++;
+//	}
+//	printf("\n");
+//	j++;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n,count=0;
+//	scanf("%d",&n);
+//	int a = n;
+//	while(n>0)
+//	{
+//		count++;
+//		n/=10;
+//	}
+//	printf("%d¢¥A %dAU¢¬¢ç ¨ùo AO¢¥I¢¥U.",a,count);
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n,sum,count=0;
+//	sum=0;
+//	scanf("%d",&n);
+//	while(n>0)
+//	{
+//		printf("%d ",n%10);
+//		sum=sum+n%10;
+//		n/=10;
+//	}
+//	printf("%d",sum);
+//}
+
+
