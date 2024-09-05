f1f89c9f2efb65d7afbca858411cd9059f580f84
avenger0115 <155057823+avenger0115@users.noreply.github.com>
2024-09-05 20:51:33 +0900

    2024-09-05

---
+++ 이름없음10.cpp
@@ -0,0 +1,106 @@
+//#include<stdio.h>
+//int main()
+//{
+//	double height[4];
+//	int j = 2;
+//	
+//	height[0] = 145.7;
+//	height[1] = 156.3;
+//	height[j] = 147.89;
+//	j++;
+//	height[j] = 159.4;
+//	printf("%lf",height[0]);
+//	return 0 ;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int ar[10] = {10,20,30,40,50,60,70,80,90,100};
+//	int br[10] = {0,};
+//	
+//	for(int i = 0;i<=9;i++)
+//	{
+//		if(i%2==1)
+//		{
+//			printf("%d ",ar[i]);
+//		}
+//	}
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int ar[10] = {5,2,1,5,6,7,8,9,4,2};
+//	int min = 1000;
+//	int index;
+//	
+//	for(int i = 0;i<=9;i++)
+//	{
+//		if(ar[i]<min)
+//		{
+//			min= ar[i];
+//			index = i;
+//		}
+//	}
+//	
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int ar[101];
+//	int n;
+//	scanf("%d",&n);
+//	for(int i = 0;i<n;i++)
+//	{
+//		scanf("%d",&ar[i]);
+//	}
+//	int v;
+//	scanf("%d",&v);
+//	int count=0;
+//	
+//	for(int i = 0;i<n;i++)
+//	{
+//		if(ar[i]==v)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d",count);
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int ar[101];
+//	int n;
+//	scanf("%d",&n);
+//	
+//	int x;
+//	scanf("%d",&x);
+//	for(int i = 0;i<n;i++)
+//	{
+//		scanf("%d",&ar[i]);
+//	}
+//	for(int i = 0;i<n;i++)
+//	{
+//		if(ar[i]<x)
+//		{
+//			printf("%d ",ar[i]);
+//		}
+//    } 
+//	
+//}
+#include<stdio.h>
+int main()
+{
+	int ar[101];
+	int n;
+	scanf("%d",&n);
+	for(int i = 0;i<n;i++)
+	{
+		scanf("%d",ar[i]);
+	}
+	
+}
+
+
+
+
